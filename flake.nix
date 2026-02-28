379c3e8f880759545e6060caf9c755b3c62f43c8
jayrovacsek <jay@rovacsek.com>
2026-02-28 22:07:27 +1100

    refactor: just expose checks packages in devshell

--- flake.nix
+++ flake.nix
@@ -103,15 +103,7 @@
         devShells.default = pkgs.devshell.mkShell {
           devshell.startup.git-hooks.text = self.checks.${system}.git-hooks.shellHook;
           name = "spectral cli shell";
-          packages =
-            (with pkgs; [
-              actionlint
-              deadnix
-              nixfmt-rfc-style
-              prettier
-              statix
-            ])
-            ++ (with self.packages.${system}; [ spectral-cli ]);
+          packages = self.checks.${system}.git-hooks.enabledPackages;
         };
 
         packages = {
